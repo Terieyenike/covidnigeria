33dbcbac078d580e2a2c7dad0d3183a0b1e489f6
Teri Eyenike <teyenike1@gmail.com>
2021-04-19 17:34:34 +0100

    added the props for the header detail

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,12 +1,12 @@
 import { Div, Section } from './Navbar.style';
 import Container from '../Container';
 
-const Navbar = ({ handleChange }) => {
+const Navbar = ({ handleChange, covid }) => {
   return (
     <Section>
       <Container>
         <Div>
-          <h1>Covid-19 in Nigeria</h1>
+          <h1>{covid}</h1>
           <input
             aria-label='Search'
             placeholder={'Search by states'}
