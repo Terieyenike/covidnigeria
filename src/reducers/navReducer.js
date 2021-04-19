8faff0ef28df6a0a0f5691d14fbe25f5ffa28341
Teri Eyenike <teyenike1@gmail.com>
2021-04-19 17:33:53 +0100

    reducer for the nav header detail

---
+++ src/reducers/navReducer.js
@@ -0,0 +1,14 @@
+import * as actions from '../actions/dataActions'
+
+export const initialState = {
+  detail: 'Report in Nigeria'
+}
+
+export default function navDetails (state = initialState, action) {
+  switch (action.type) {
+    case actions.NAV_DETAIL:
+      return {...state, detail: state.detail}
+    default:
+      return state
+  }
+}
